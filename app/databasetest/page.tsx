d65e74fb7091455346fc5f792b0b71331fe092d5
Baz Murphy <bazmurphy@gmail.com>
2023-06-05 19:39:28 +0100

    added prisma client, and database test page, enabled server actions

---
+++ app/databasetest/page.tsx
@@ -0,0 +1,49 @@
+// this is a (default) server component
+// it can call prisma directly (without the client component request/response cycle)
+// the component fetches the data before it is sent to the client
+
+import { prisma } from "../libs/prismadb";
+import "./databasetest.css";
+import timestampFormatter from "./timestampformatter";
+
+export default async function DatabaseTest() {
+  // an async Server Action function to get all of the Todos
+  async function getAllTodos() {
+    "use server";
+    const allTodos = await prisma.todo.findMany();
+    // note: this console log shows in the server console
+    console.log("getAllTodos allTodos:", allTodos);
+    return allTodos;
+  }
+
+  const allTodos = await getAllTodos();
+
+  return (
+    <div className="databasetest-page">
+      <h1 className="databasetest-title">Database Test</h1>
+      <ul className="todo-list">
+        {allTodos.length > 0 &&
+          allTodos.map((todo) => {
+            return (
+              <li key={todo.id} className="todo-list-item">
+                <span className="todo-list-item-key">id:</span>
+                <span>{todo.id}</span>
+                <span className="todo-list-item-key">title:</span>
+                <span>{todo.title}</span>
+                <span className="todo-list-item-key">content:</span>
+                <span>{todo.content}</span>
+                <span className="todo-list-item-key">priority:</span>
+                <span>{todo.priority}</span>
+                <span className="todo-list-item-key">complete:</span>
+                <span>{todo.complete.toString()}</span>
+                <span className="todo-list-item-key">createdAt:</span>
+                <span>{timestampFormatter(todo.createdAt)}</span>
+                <span className="todo-list-item-key">updatedAt:</span>
+                <span>{timestampFormatter(todo.createdAt)}</span>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
---
+++ app/databasetest/timestampformatter.ts
@@ -0,0 +1,14 @@
+export default function timestampFormatter(date: Date) {
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour12: false,
+    timeZone: "GMT",
+  };
+  const formatter = new Intl.DateTimeFormat("en-GB", options);
+  return formatter.format(date);
+}

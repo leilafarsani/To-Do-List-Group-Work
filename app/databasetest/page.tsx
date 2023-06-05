4b116aa262e70d3b9fc539f120289f390147838e
Baz Murphy <bazmurphy@gmail.com>
2023-06-05 20:44:09 +0100

    some conditional rendering and styling

--- app/databasetest/page.tsx
+++ app/databasetest/page.tsx
@@ -7,11 +7,11 @@ import "./databasetest.css";
 import timestampFormatter from "./timestampformatter";
 
 export default async function DatabaseTest() {
-  // an async Server Action function to get all of the Todos
+  // an async Server Action function to get all of the Todos from the database
   async function getAllTodos() {
     "use server";
     const allTodos = await prisma.todo.findMany();
-    // note: this console log shows in the server console
+    // note: this console.log shows in the server console (not the client)
     console.log("getAllTodos allTodos:", allTodos);
     return allTodos;
   }
@@ -27,15 +27,24 @@ export default async function DatabaseTest() {
             return (
               <li key={todo.id} className="todo-list-item">
                 <span className="todo-list-item-key">id:</span>
-                <span>{todo.id}</span>
+                <span className="todo-list-item-id">{todo.id}</span>
                 <span className="todo-list-item-key">title:</span>
                 <span>{todo.title}</span>
                 <span className="todo-list-item-key">content:</span>
                 <span>{todo.content}</span>
                 <span className="todo-list-item-key">priority:</span>
-                <span>{todo.priority}</span>
+                <span>
+                  {todo.priority}{" "}
+                  {todo.priority === "low"
+                    ? "🟡"
+                    : todo.priority === "medium"
+                    ? "🟠"
+                    : "🔴"}
+                </span>
                 <span className="todo-list-item-key">complete:</span>
-                <span>{todo.complete.toString()}</span>
+                <span>
+                  {todo.complete.toString()} {todo.complete ? "✅" : "❌"}
+                </span>
                 <span className="todo-list-item-key">createdAt:</span>
                 <span>{timestampFormatter(todo.createdAt)}</span>
                 <span className="todo-list-item-key">updatedAt:</span>
